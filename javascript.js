109346dfaf7ca0addf7898224a3488f5a2c7a87c
Andoni Sarasua <71468125+AndoniSarasua@users.noreply.github.com>
2023-07-10 12:23:20 -0300

    Update javascript.js
    Archivo Js modificado para la entrega de DOM

--- javascript.js
+++ javascript.js
@@ -1,18 +1,12 @@
-for(let i=1; i<5; i++){
-let precio_cliente = prompt ("Ingrese el precio de su prenda que desea vender en USD:")
-let cantidad_cliente = prompt ("Ingrese la cantidad que desea vender:")
+const stock = [1,2,3,4,5,6];
 
-function calcular_precio(precio, cantidad){
-    precio_final= precio * cantidad - (precio*cantidad)*0.1
-}
-
-if((precio_cliente && cantidad_cliente)>0){
-calcular_precio(precio_cliente, cantidad_cliente)
-console.log ("Lo que usted obtendra al vender su prenda seran ",precio_final,"USD")
-}
-else{
-console.log("Ingrese un precio y cantidad superior a 0")
+for(let i=0; i<6; i++){
+stock[i]  = prompt ("Ingrese el stock del producto numero:",i)
+console.log(stock[i])
+const id = document.getElementById(i)
+if (stock[i]<=0){
+    console.log("NO HAY STOCK")
+    id.remove()
 }
 }
-//En el codigo la idea es lograr un algoritmo el cual calcule el precio final que le quedara al usuario al momento de vender en nuestra pagina y ser cobrada la comision de venta.
-//Por el lado del ciclo no se me ocurrieron muchas ideas funcionales, pero use un for para que el ciclo se ejecute un numero especifico de veces.
+//Algoritmo el cual elimina el producto que no tenga stock. Todo trabajado sobre los productos destacados del index.
